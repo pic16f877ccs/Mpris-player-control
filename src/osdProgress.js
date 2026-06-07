a8597822421852eecaf5f630d7f142c4047eb680
Karl Wulfert <pic16f877.ccs@emailn.de>
2026-06-07 21:01:02 +0200

    Refactor ProgressBarLevel to use BarLevel from GNOME Shell UI

--- src/osdProgress.js
+++ src/osdProgress.js
@@ -12,191 +12,19 @@
  * (at your option) any later version.
  */
 
-import Atk from 'gi://Atk';
 import Clutter from 'gi://Clutter';
 import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
 import St from 'gi://St';
 
+import * as BarLevel from 'resource:///org/gnome/shell/ui/barLevel.js';
 import * as Layout from 'resource:///org/gnome/shell/ui/layout.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 const HIDE_TIMEOUT = 1500;
 const FADE_TIME = 100;
 export const LEVEL_ANIMATION_TIME = 100;
-const MAX_US = Number.MAX_SAFE_INTEGER;
-
-export const ProgressBarLevel = GObject.registerClass({
-    Properties: {
-        'value': GObject.ParamSpec.double(
-            'value',
-            null,
-            null,
-            GObject.ParamFlags.READWRITE,
-            0,
-            MAX_US,
-            0
-        ),
-        'maximum-value': GObject.ParamSpec.double(
-            'maximum-value',
-            null,
-            null,
-            GObject.ParamFlags.READWRITE,
-            1,
-            MAX_US,
-            1
-        ),
-    },
-}, class ProgressBarLevel extends St.DrawingArea {
-    _init(params = {}) {
-        this._maxValue = 1;
-        this._value = 0;
-        this._barLevelWidth = 0;
-        this._barLevelHeight = 0;
-        this._barLevelColor = null;
-        this._barLevelActiveColor = null;
-
-        super._init({
-            style_class: 'level',
-            accessible_role: Atk.Role.LEVEL_BAR,
-            ...params,
-        });
-
-        this.connect('notify::allocation', () => {
-            this._barLevelWidth = this.allocation.get_width();
-        });
-
-        this._customAccessible = St.GenericAccessible.new_for_actor(this);
-        this.set_accessible(this._customAccessible);
-
-        this._customAccessible.connect('get-current-value', () => this._value);
-        this._customAccessible.connect('get-minimum-value', () => 0);
-        this._customAccessible.connect('get-maximum-value', () => this._maxValue);
-        this._customAccessible.connect('set-current-value', (_actor, value) => {
-            this.value = value;
-        });
-
-        this.connect('notify::value', () => {
-            this._customAccessible.notify('accessible-value');
-        });
-    }
-
-    get value() {
-        return this._value;
-    }
-
-    set value(value) {
-        value = Math.max(Math.min(value, this._maxValue), 0);
-
-        if (this._value === value)
-            return;
-
-        this._value = value;
-        this.notify('value');
-        this.queue_repaint();
-    }
-
-    get maximumValue() {
-        return this._maxValue;
-    }
-
-    set maximumValue(value) {
-        value = Math.max(value, 1);
-
-        if (this._maxValue === value)
-            return;
-
-        this._maxValue = value;
-        this.notify('maximum-value');
-        this.queue_repaint();
-    }
-
-    vfunc_style_changed() {
-        const themeNode = this.get_theme_node();
-        this._barLevelHeight = themeNode.get_length('-barlevel-height');
-        this._barLevelColor = themeNode.get_color('-barlevel-background-color');
-        this._barLevelActiveColor =
-            themeNode.get_color('-barlevel-active-background-color');
-
-        super.vfunc_style_changed();
-    }
-
-    vfunc_repaint() {
-        const cr = this.get_context();
-        const themeNode = this.get_theme_node();
-        const [width, height] = this.get_surface_size();
-        const rtl = this.get_text_direction() === Clutter.TextDirection.RTL;
-
-        const radius = Math.min(width, this._barLevelHeight) / 2;
-        const TAU = Math.PI * 2;
-
-        let progress = 0;
-        if (this._maxValue > 0)
-            progress = this._value / this._maxValue;
-
-        if (rtl)
-            progress = 1 - progress;
-
-        const endX = radius + (width - 2 * radius) * progress;
-
-        let startArcX = radius;
-        let endArcX = width - radius;
-        if (rtl)
-            [startArcX, endArcX] = [endArcX, startArcX];
-
-        if (!rtl)
-            cr.arc(endArcX, height / 2, radius, TAU * (3 / 4), TAU * (1 / 4));
-        else
-            cr.arcNegative(endArcX, height / 2, radius, TAU * (3 / 4), TAU * (1 / 4));
-
-        cr.lineTo(endX, (height + this._barLevelHeight) / 2);
-        cr.lineTo(endX, (height - this._barLevelHeight) / 2);
-        cr.lineTo(endArcX, (height - this._barLevelHeight) / 2);
-        cr.setSourceColor(this._barLevelColor);
-        cr.fill();
-
-        if (this._value > 0) {
-            if (!rtl)
-                cr.arc(startArcX, height / 2, radius, TAU * (1 / 4), TAU * (3 / 4));
-            else
-                cr.arcNegative(startArcX, height / 2, radius, TAU * (1 / 4), TAU * (3 / 4));
-
-            cr.lineTo(endX, (height - this._barLevelHeight) / 2);
-            cr.lineTo(endX, (height + this._barLevelHeight) / 2);
-            cr.lineTo(startArcX, (height + this._barLevelHeight) / 2);
-            cr.setSourceColor(this._barLevelActiveColor);
-            cr.fill();
-
-            if (!rtl) {
-                cr.arc(endX, height / 2, radius, TAU * (3 / 4), TAU * (1 / 4));
-                cr.lineTo(Math.floor(endX), (height + this._barLevelHeight) / 2);
-                cr.lineTo(Math.floor(endX), (height - this._barLevelHeight) / 2);
-            } else {
-                cr.arcNegative(endX, height / 2, radius, TAU * (3 / 4), TAU * (1 / 4));
-                cr.lineTo(Math.ceil(endX), (height + this._barLevelHeight) / 2);
-                cr.lineTo(Math.ceil(endX), (height - this._barLevelHeight) / 2);
-            }
-
-            cr.lineTo(endX, (height - this._barLevelHeight) / 2);
-            cr.setSourceColor(this._barLevelActiveColor);
-            cr.fill();
-        }
-
-        cr.$dispose();
-    }
-
-    vfunc_get_preferred_height(_forWidth) {
-        const themeNode = this.get_theme_node();
-        const height = this._barLevelHeight;
-        return themeNode.adjust_preferred_height(height, height);
-    }
-
-    vfunc_get_preferred_width(_forHeight) {
-        const themeNode = this.get_theme_node();
-        const width = 100;
-        return themeNode.adjust_preferred_width(width, width);
-    }
-});
+export const ProgressBarLevel = BarLevel.BarLevel;
 
 export const OsdProgressWindow = GObject.registerClass(
 class OsdProgressWindow extends Clutter.Actor {
